d91c2fb3cc217a10eb8549bec1fe72e0d4b23be0
Sina James <sina.james@SJAMES.local>
2016-04-13 11:08:16 -0500

    fixing some spacing

--- lib/elixir_status/meta.ex
+++ lib/elixir_status/meta.ex
@@ -7,7 +7,7 @@ defmodule ElixirStatus.Meta do
     Elixir news and status updates from the community for Elixir and Phoenix
   """
 
-  def html_title, do: @description
+  def html_title,       do: @description
   def html_description, do: @description
-  def rss_title, do: @description
+  def rss_title,        do: @description
 end
